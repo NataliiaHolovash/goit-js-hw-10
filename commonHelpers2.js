17732a77e5c8481e6c7d58ca5b81729cdfdd9ed3
NataliiaHolovash <151832421+NataliiaHolovash@users.noreply.github.com>
2024-05-17 21:16:29 +0000

    Deploying to gh-pages from @ NataliiaHolovash/goit-js-hw-10@cffc28f8455109f074b9d3ffc273da21d0855a28 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{i}from"./assets/vendor-77e16229.js";const r=document.querySelector(".form");r.addEventListener("submit",t=>{t.preventDefault(),new Promise((e,o)=>{const s=t.target.delay.value;setTimeout(()=>{t.target.state.value==="fulfilled"?e(s):o(s)},s)}).then(e=>{i.success({message:`✅ Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{i.error({message:`❌ Rejected promise in ${e}ms`,position:"topRight"})})});
+//# sourceMappingURL=commonHelpers2.js.map
